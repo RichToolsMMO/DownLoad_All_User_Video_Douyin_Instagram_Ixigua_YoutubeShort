65d34b8da232f90334a06fb13c307537e49b2a67
RichToolsMMO <156527010+RichToolsMMO@users.noreply.github.com>
2024-10-25 22:52:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+Software to download all user short videos from TikTok, Douyin, Instagram, Ixigua, and YouTube Shorts without any watermarks. (iOS, Android)
 
+Contact:
+• WhatsApp: https://wa.me/84777286725
+• Telegram: https://t.me/richsoftwaremmo
